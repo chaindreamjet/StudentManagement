555f319566099f8586afbf7e75271a15422852c5
shixing <249978022@qq.com>
2018-12-06 19:13:21 +0800

    删除一些测试用的非必要代码，以及添加package.json文件

--- server/server.js
+++ server/server.js
@@ -12,15 +12,6 @@ var app = express();
 var query_sql = query_0.query;
 var add_sql = adddb.addDB;
 
-//console.log(query_sql)
-
-//console.log(adddb)
-//console.log(add_sql)
-/*
-app.get('/login_page',(req,res)=>{//For login's page router;
-    res.send('bibi');
-})
-*/
 app.use(bparse.urlencoded({extended:true}))
 app.use(express.static('public'));
 app.get('/index.html',(req,res)=>{
@@ -30,8 +21,7 @@ app.get('/index.html',(req,res)=>{
 app.get('/students',(req,res)=>{
     var querysentence = 'select * from student1;';
     add_sql(querysentence).then((ans)=>{
-        //console.log(ans)
-        res.send(JSON.stringify(ans))
+        res.end(JSON.stringify(ans))
     });
 })
 app.post('/add_stu',(req,res)=>{//For home page's router; 
@@ -40,15 +30,12 @@ app.post('/add_stu',(req,res)=>{//For home page's router;
        'add_info':    req.body
     }
     console.log(stu_info.add_info)
-    //console.log(req)
     var querysentence = 'insert into student1'
     var values = "('"
     var keys = '('
     for(var key in stu_info.add_info) {
-       // console.log(key)
         keys += key
         values += stu_info.add_info[key]
-        // querysentence += "'" + stu_info.add_info[key] + "'"; 
         if(key === 'stime'){ 
             keys += ')'
             values += "')"
@@ -57,7 +44,6 @@ app.post('/add_stu',(req,res)=>{//For home page's router;
             keys += ','
             values += "','"
         }
-        //console.log(querysentence)
     }
 
     querysentence += keys + ' values' + values + ';' 
@@ -66,12 +52,9 @@ app.post('/add_stu',(req,res)=>{//For home page's router;
     console.log(querysentence)      
     add_sql(querysentence).then(()=>{
         add_sql(querysentence_2).then((ans)=>{
-            //console.log(ans)
-            res.send(JSON.stringify(ans))
+            res.end(JSON.stringify(ans))
         })
     });
-    //console.log(querysentence)
-    //console.log('insert success.')
     
 })
 app.post('/search_stu',(req,res)=>{
@@ -80,7 +63,7 @@ app.post('/search_stu',(req,res)=>{
     add_sql(querysentence).then((ans)=>{
         
             console.log(ans)
-            res.send(JSON.stringify(ans))
+            res.end(JSON.stringify(ans))
         
     })
 })
@@ -88,59 +71,34 @@ app.post('/delete_stu',(req,res)=>{
     
     var snum = req.body;
     var snum_0;
-    //console.log(JSON.stringify(snum))
      for(var key in snum){
-    //     //console.log(key)
          snum_0=key;
      }
     console.log(snum_0)
-    //console.log(snum);
     var querysentence_2 = 'select * from student1'
     var querysentence = "delete from student1 where snum='"+snum_0+"';";
     console.log(querysentence)
     add_sql(querysentence).then((ans)=>{
         add_sql(querysentence_2).then((ans)=>{
-           // console.log(ans)
-            res.send(JSON.stringify(ans))
+            res.end(JSON.stringify(ans))
         })
     })
-
-    //res.end();
 })
 
 app.post('/change_stu',(req,res)=>{
     var sname_0 = req.body 
     var querysentence_2 = 'select * from student1'
-    //console.log(sname);
     var querysentence = "update student1 set sname='"+sname_0.sname+"',snum='"+sname_0.snum+"',sschool='"+sname_0.sschool+"',sclass='"+sname_0.sclass+"',sgender='"+sname_0.sgender+"',smajor='"+sname_0.smajor+"' where snum='"+sname_0.searchnum+"';"
     console.log(querysentence)
     add_sql(querysentence).then((ans)=>{
         add_sql(querysentence_2).then((ans)=>{
-            //console.log(ans)
-            res.send(JSON.stringify(ans))
+            res.end(JSON.stringify(ans))
         })
     })
 })
 
 
-//app.get('/serch',(req,res)=>{//For the function of searchingl
-
-//})
-// for test demo
-//query_sql.queryName('select * from manager1;').then((ans)=>{
-
-//    console.log(ans);
-//})
-
-//res.end('test')
 const hostname = 'localhost'
 app.listen(3000,hostname,()=>{
     console.log("connecting to port 3000...")
-    //query_0.query.queryName('gggg')
-    
-
 })
-/*
-(function(){
-})()
-*/
